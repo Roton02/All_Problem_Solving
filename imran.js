f849298131b3f23653756c5d14a1fcb0392f3c61
Roton02 <rph645102@gmail.com>
2024-08-21 18:11:56 +0600

    cheeked new problem

---
+++ imran.js
@@ -0,0 +1,7 @@
+console.log("imran" && "raju")
+console.log(typeof(+"5"+4))
+console.log("5"+3)
+console.log("imran" || "raju")
+console.log("imran" && "raju")
+console.log(+"5"+4)
+console.log(typeof ("5" * 3));
